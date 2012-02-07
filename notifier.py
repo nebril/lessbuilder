67b1681dcf18a5983b3f02ba5812b84ef954f7d5
Nebril <maciej.iai@gmail.com>
2012-02-08 00:39:01 +0100

    Changing path to generic

--- notifier.py
+++ notifier.py
@@ -5,26 +5,21 @@ import os
 
 class MyEventHandler(pyinotify.ProcessEvent):
     def process_IN_MODIFY(self, event):
-        result = commands.getstatusoutput('/home/nebril/node_modules/less/bin/lessc ' + event.pathname + ' ../' + event.path[:-4] + 'css');
+        result = commands.getstatusoutput('#lessc path#/lessc ' + event.pathname + ' ../' + event.path[:-4] + 'css');
         if(result[0] == 0) :
             print 'compile successful'
         else :
             print result[1]
 
-def main():
-    fileList = os.listdir(os.getcwd())
+
+fileList = os.listdir(os.getcwd())
+wm = pyinotify.WatchManager()
+for fname in fileList :
+    if(fname[-4:] == 'less'):
+        wm.add_watch(fname, pyinotify.IN_MODIFY, rec=True)
 
 
-    wm = pyinotify.WatchManager()
-    for fname in fileList :
-        if(fname[-4:] == 'less'):
-            wm.add_watch(fname, pyinotify.IN_MODIFY, rec=True)
+eh = MyEventHandler()
 
-
-    eh = MyEventHandler()
-
-    notifier = pyinotify.Notifier(wm, eh)
-    notifier.loop()
-
-if __name__ == '__main__':
-    main()
+notifier = pyinotify.Notifier(wm, eh)
+notifier.loop()
